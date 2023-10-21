68e77ef5ce24186ec89c78503236855cba2df162
PSKutergin <PSKutergin@gmail.com>
2023-10-21 14:44:45 +0300

    add sort services

--- src/admin.js
+++ src/admin.js
@@ -3,9 +3,11 @@ import modalService from "./modules/modalService";
 import addService from "./modules/addService";
 import editService from "./modules/editService";
 import removeService from "./modules/removeService";
+import sortService from "./modules/sortService";
 
 renderTable();
 modalService();
 addService();
 editService();
 removeService();
+sortService();
---
+++ src/modules/sortService.js
@@ -0,0 +1,55 @@
+import getData from "./getData";
+import renderData from "./renderData";
+
+const sortService = () => {
+    const selector = document.getElementById('typeItem')
+    const thead = document.querySelector('.table > thead');
+    const url = 'http://localhost:4545/repairs?_sort=';
+
+    let currentType;
+    let colName;
+    let isSort = false;
+
+    thead.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        if (e.target.closest('.th-id')) {
+            colName = 'id'
+        } else if (e.target.closest('.th-type')) {
+            colName = 'type'
+        } else if (e.target.closest('.th-name')) {
+            colName = 'name'
+        } else if (e.target.closest('.th-units')) {
+            colName = 'units'
+        };
+
+        if (isSort) {
+            e.target.querySelector('svg').style.transform = 'rotate(180deg)';
+        } else {
+            e.target.querySelector('svg').style.transform = '';
+        };
+
+        const sortOptions = {
+            name: colName,
+            value: isSort ? 'asc' : 'desc'
+        };
+
+        getData(`${url}${sortOptions.name}&_order=${sortOptions.value}`)
+            .then(data => {
+                currentType = selector.options[selector.selectedIndex].innerHTML
+
+                if (currentType === 'Все услуги') {
+                    renderData(data)
+                } else {
+                    let list = data.filter((item) => item.type === currentType);
+                    renderData(list)
+                };
+            });
+
+        isSort = !isSort;
+    })
+
+
+}
+
+export default sortService
